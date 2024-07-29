ba0c1591f3e97f5e670b6a886646358536b5ef3e
Niklas <niklas.buk@gmail.com>
2024-07-29 20:45:53 +0700

    fix: Comment match property on User model, which cause validation mistakes during the authorization. Create TODO fo fix this later

--- models/user.js
+++ models/user.js
@@ -8,11 +8,12 @@ const UserSchema = new Schema({
 	},
 	username: {
 		type: String,
-		required: [true, 'Username is required'],
-		match: [
-			/^(&=.{8,20}$)(&![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-			'Usename invalid, it should contain 8-20 alphanumeric letters and be unique!'
-		]
+		required: [true, 'Username is required']
+		// TODO: fix match
+		// match: [
+		// 	/^(&=.{8,20}$)(&![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+		// 	'Usename invalid, it should contain 8-20 alphanumeric letters and be unique!'
+		// ]
 	},
 	image: {
 		type: String
