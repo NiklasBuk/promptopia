6b679c9558690c5ad0354856caef765d313e0df8
Niklas <niklas.buk@gmail.com>
2024-07-29 22:39:45 +0700

    feat: Add conditional layout for edit and delete handlers

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -3,9 +3,12 @@
 import { useState } from 'react'
 import Image from 'next/image'
 import { useSession } from 'next-auth/react'
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+	const { data: session } = useSession()
+	const pathName = usePathname()
+
 	const [copied, setCopied] = useState('')
 
 	const handleCopy = () => {
@@ -54,8 +57,24 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 				className='font-inter text-sm blue_gradient cursor-pointer'
 				onClick={() => handleTagClick && handleTagClick(post.tag)}
 			>
-				{post.tag}
+				#{post.tag}
 			</p>
+			{session?.user.id === post.creator._id && pathName === '/profile' && (
+				<div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+					<p
+						className='font-inteer text-sm blue_gradient cursor-pointer'
+						onClick={handleEdit}
+					>
+						Edit
+					</p>
+					<p
+						className='font-inteer text-sm orange_gradient cursor-pointer'
+						onClick={handleDelete}
+					>
+						Delete
+					</p>
+				</div>
+			)}
 		</div>
 	)
 }
