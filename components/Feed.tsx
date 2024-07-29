4621b87d7f1bcb8ade3c7af10fcf791d90dc3df3
Niklas <niklas.buk@gmail.com>
2024-07-29 20:27:32 +0700

    feat: Finish Feed and PromptCard components

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,53 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+import PromptCard from '@components/PromptCard'
+
+const PromptCardList = ({ data, handleTagClick }) => {
+	return (
+		<div className='mt-16 prompt_layout'>
+			{data.map(prompt => (
+				<PromptCard
+					key={prompt._id}
+					post={prompt}
+					handleTagClick={handleTagClick}
+				/>
+			))}
+		</div>
+	)
+}
+
 const Feed = () => {
-	return <div></div>
+	const [searchText, setSearchText] = useState('')
+	const [posts, setPosts] = useState([])
+
+	const handleSearchChange = e => {}
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch('/api/prompt')
+			const data = await response.json()
+
+			setPosts(data)
+		}
+	}, [])
+
+	return (
+		<section className='feed'>
+			<form className='relative w-full flex-center'>
+				<input
+					type='text'
+					placeholder='Search for a tag or a username'
+					value={searchText}
+					onChange={handleSearchChange}
+					required
+					className='search_input peer'
+				/>
+			</form>
+			<PromptCardList data={posts} handleTagClick={() => {}} />
+		</section>
+	)
 }
 
 export default Feed
